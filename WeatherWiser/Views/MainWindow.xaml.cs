2e888baa1229a6254f8299dc2432e08e1a544edd
Manabu Kikuchi <manabu.kikuchi.mobile@gmail.com>
2025-02-27 00:46:28 +0900

    updated.

--- WeatherWiser/Views/MainWindow.xaml.cs
+++ WeatherWiser/Views/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Threading.Channels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
